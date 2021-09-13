8ecbc896385bb79a24ef79a3fe2d711e3e628906
HyeonseongLee <b01099834850@gmail.com>
2021-09-13 20:57:39 +0900

    컴퓨터프로그래밍 2주차

---
+++ 2주차/Ex.py
@@ -0,0 +1,8 @@
+a=34
+b="Made in Korea"
+list_data=["a","b","c"]
+dic_data={"현성" : 1 , "이" : 2}
+print(a)
+print(b)
+print(list_data)
+print(dic_data[1])
---
+++ 2주차/input.py
@@ -0,0 +1,3 @@
+a=input("입력바람")
+
+print(a)
